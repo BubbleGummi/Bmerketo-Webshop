834ce8bc09f03c73775010467d37d1a5b1f3cfce
Diana Hogescu <diana.hogescu@learnet.se>
2023-08-20 22:07:28 +0200

    Add project files.

---
+++ WebApp/Migrations/User/20230820184344_Worki.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations.User
+{
+    /// <inheritdoc />
+    public partial class Worki : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebApp/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models.Contexts;
+using WebApp.Models.Identities;
+
+namespace WebApp.Services
+{
+    public class UserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserService(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IEnumerable<(string email, string firstName, string lastName, string roleName)>> GetAllUsersWithRolesAsync()
+        {
+            var userAndRole = new List<(string email, string firstName, string lastName, string roleName)>();
+            var users = await _userManager.Users.ToListAsync();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                var roleName = roles.FirstOrDefault();
+
+                userAndRole.Add((
+                    email: user.Email ?? "",   
+                    firstName: user.FirstName,
+                    lastName: user.LastName,
+                    roleName: roleName ?? ""      
+                ));
+            }
+
+            return userAndRole;
+        }
+    }
+
+}
